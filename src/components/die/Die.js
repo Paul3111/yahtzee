4c716a7a87db6892441135df6acdf471e5f610c3
Josh Neuwford <josh.c.gosling@gmail.com>
2023-04-25 13:00:21 +0100

    added dice images and array

--- src/components/die/Die.js
+++ src/components/die/Die.js
@@ -1,37 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const Die = () => {
-  const [value, setValue] = useState(1);
   const diceImages = [
-    "./assets/dice-1.png",
-    "./assets/dice-2.png",
-    "./assets/dice-3.png",
-    "./assets/dice-4.png",
-    "./assets/dice-5.png",
-    "./assets/dice-6.png"
+    "./assets/dice-one.png",
+    "./assets/dice-two.png",
+    "./assets/dice-three.png",
+    "./assets/dice-four.png",
+    "./assets/dice-five.png",
+    "./assets/dice-six.png"
   ];
-
-  const rollDie = () => {
-    const result = Math.floor(Math.random() * 6) + 1;
-    setValue(result);
-  }
-
-  return (
-    <div onClick={rollDie}>
-      <img
-        className={`dice--image ${
-          value === 1 ? `dice__image__one` :
-          value === 2 ? `dice__image__two` :
-          value === 3 ? `dice__image__three` :
-          value === 4 ? `dice__image__four` :
-          value === 5 ? `dice__image__five` :
-          `dice__image__six`
-        }`}
-        src={diceImages[result -1]}
-        alt={`Dice value: ${result}`}
-      />
-    </div>
-  )
 }
 
 export default Die;
