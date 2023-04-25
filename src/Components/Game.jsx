4bc2a8ea885e2d5995084864cab669607d32eb83
ChangWynn <huynhchang.one@gmail.com>
2023-04-25 12:50:49 +0100

    style body

---
+++ src/Components/Game.jsx
@@ -0,0 +1,12 @@
+import style from './Game.module.css'
+
+
+const Game = () => {
+  return (
+    <div className={style['game-container']}>
+      <h1>Title</h1>
+    </div>
+  );
+};
+
+export default Game;
