c8b9f003e04d0095ad372bbaac42a87a394c4b2a
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-04-26 18:53:34 +0100

    updates

--- api/app.js
+++ api/app.js
@@ -2,19 +2,22 @@ const express = require('express');
 const path = require("path");
 //const logger = require("morgan");
 const app = express();
+const cors = require('cors');
 
 const players = require('./routes/players.js')
 
+app.use(cors());
+
 // setup for receiving JSON
-app.use(express.json())
 
 //app.use(logger("dev"));
-//app.use(express.json());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-    console.log('HIIIIIIIIIII')
-    res.send('HELLO')
-})
+app.use('/', players);
 
-app.use('/api', players);
+app.listen(8080, function() {
+    console.log('Server listening on port 8080');
+});
+
+module.exports = app;
--- api/controllers/players.js
+++ api/controllers/players.js
@@ -1,14 +1,23 @@
 const Player = require('../models/player');
+const { ObjectId } = require("mongodb");
 
 const PlayersController = {
     Index: (_req, res) => {
-        Player.find(async (err, players) => {
-            if (err) {
-                throw err;
-            }
-            res.status(200).json({players: players});
-        });
-    }
-};
+        console.log('IN THE CONTROLLER')
+
+        res.status(200).json({data: [
+            {username: "Paul", score: [100]},
+            {username: "Chang", score: [100]}
+        ]});
+
+    //     Player.find(async (err, players) => {
+    //         console.log('IN THE METHOD')
+    //         if (err) {
+    //             throw err;
+    //         }
+    //         res.status(200).json({players: players});
+    //     });
+    // }
+}};
 
 module.exports = PlayersController;
