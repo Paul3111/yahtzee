64dbbd51d1dad0f96d3d7e40b86b6bd9c270d877
Josh Neuwford <josh.c.gosling@gmail.com>
2023-04-26 10:48:56 +0100

    added props and code for rolling animation with placeholder CSS

--- src/dice/DiceContainer.js
+++ src/dice/DiceContainer.js
@@ -11,23 +11,23 @@ const DiceContainer = () => {
     { value: 1, locked: false }
   ]);
 
-    //console log functions ------------
-    const logRolls = (rolls) => {
-      let r = [];
-      rolls.forEach(die => {
-        r.push(die.value);
-      })
-      console.log(r);
-    }
+  //console log functions ------------
+  const logRolls = (rolls) => {
+    let r = [];
+    rolls.forEach(die => {
+      r.push(die.value);
+    })
+    console.log(r);
+  }
 
-    const logLockedState = (rolls) => {
-      let l = [];
-      rolls.forEach(die => {
-        l.push(die.locked);
-      })
-      console.log(l)
-    }
-    // ---------------------------------
+  const logLockedState = (rolls) => {
+    let l = [];
+    rolls.forEach(die => {
+      l.push(die.locked);
+    })
+    console.log(l)
+  }
+  // ---------------------------------
 
 
   // toggles dice state between locked and unlocked
@@ -40,9 +40,15 @@ const DiceContainer = () => {
 
   // rolls dice that are not locked and updates the dice values
   const rollDice = () => {
-    const rolledDice = dice.map(die => (die.locked ? die : { ...die, value: rollDie()}));
+    const rolledDice = dice.map(die => (die.locked ? die : { ...die, rolling: true, value: rollDie()}));
     setDice(rolledDice);
-    logRolls(rolledDice)
+    logRolls(rolledDice);
+
+    setTimeout(() => {
+      const finishedDice = rolledDice.map(die => ({ ...die, rolling: false }));
+      setDice(finishedDice);
+    }, 1000);
+
     }
   
   // returns a random number 1 - 6
@@ -54,7 +60,7 @@ const DiceContainer = () => {
     <div className="wrapper">
       <div className="dice-container">
         { dice.map((die, index) => (
-          <Die key={index} value={die.value} locked={die.locked} onClick={() => toggleLock(index)}/>
+          <Die key={index} value={die.value} locked={die.locked} rolling={die.rolling} onClick={() => toggleLock(index)}/>
         ))}
       </div>
       <button className="roll-button" onClick={rollDice}>Roll Dice</button>
--- src/dice/Die.js
+++ src/dice/Die.js
@@ -3,11 +3,11 @@ import './Die.css'
 
 const Die = (props) => {
   // this props declaration makes JSX more readable and easier to add parent components!
-  const { value, locked, onClick } = props
+  const { value, locked, onClick, rolling } = props
 
   // images files for dice need to be formatted to contain a number.
   return (
-    <div className={`die ${locked ? "locked" : ""}`} onClick={onClick}>
+    <div className={`die ${locked ? "locked" : ""} ${rolling ? "rolling" : ""}`} onClick={onClick}>
       <img src={`./assets/dice-${value}.png`} width="70" alt=""/>
     </div>
   )
