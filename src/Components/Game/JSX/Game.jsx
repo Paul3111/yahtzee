cba0bb4ad6100b7bcd2da343c6200d6ec2c339da
Josh Neuwford <josh.c.gosling@gmail.com>
2023-04-27 11:03:16 +0100

    added props and function to send dice values to game.jsx

--- src/Components/Game/JSX/Game.jsx
+++ src/Components/Game/JSX/Game.jsx
@@ -1,30 +1,44 @@
-import {useState, useEffect} from 'react'
-
+import {useEffect, useState} from 'react'
 import style from '../CSS/Game.module.css';
 import GameHeader from './GameHeader';
 import GameRooms from './GameRooms';
 import HowToPlay from '../../HowToPlay/JSX/HowToPlay';
 import GameTotalScore from './GameTotalScore';
 import DiceContainer from '../../Dice/DiceContainer';
-
 const Game = () => {
-  const [isHovered, setIsHovered] = useState(false) 
-
+  const [isHovered, setIsHovered] = useState(false)
+  const [counts, setCounts] = useState([0, 0, 0, 0, 0, 0]);
+  const [diceValueSum, setDiceValueSum] = useState(0);
+  const [values, setValues] = useState([])
+  const [dice, setDice] = useState([
+    { value: 1, locked: false },
+    { value: 1, locked: false },
+    { value: 1, locked: false },
+    { value: 1, locked: false },
+    { value: 1, locked: false }
+  ]);
   const hoverHandler = () => {
     setIsHovered(!isHovered)
   }
-
+  //console.log("sum: ",  diceValueSum);
+  //console.log("counts",  counts);
+  //console.log("values", values)
+  
   return (
     <div className={style['god-container']}>
       <GameHeader />
       <div className={`${style['game-container']} ${isHovered && style['lights-up']}`}>
         <GameTotalScore isHovered={isHovered} />
-        <GameRooms />  
-        <DiceContainer onBtnHover={hoverHandler} />
+        <GameRooms dice={dice}/>
+        <DiceContainer onBtnHover={hoverHandler}
+          dice={dice}
+          setDice={setDice}
+          counts={counts} setCounts={setCounts}
+          diceValueSum={diceValueSum} setDiceValueSum={setDiceValueSum}
+          values={values} setValues={setValues}/>
       </div>
         <HowToPlay />
     </div>
   );
 };
-
 export default Game;
