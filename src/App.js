15835d215fa1d4e858eabaa6d1423143ae91adc5
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-04-27 20:54:15 +0100

    Added settings route and button.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Game from './Components/Game/JSX/Game';
 import Home from './Components/Home/Home';
 import Scoreboard from './Components/Scoreboard/JSX/Scoreboard';
+import Settings from './Components/Settings/JSX/Settings';
 
 import {
   useNavigate,
@@ -16,7 +17,7 @@ const App = () => {
       <Route path="/home" element={<Home navigate={ useNavigate()} />} />
       <Route path="/game" element={<Game navigate={ useNavigate()} />} />
       <Route path="/scoreboard" element={<Scoreboard navigate={ useNavigate()} />} />
-      <Route path="/settings" element={<Game navigate={ useNavigate()} />} />
+      <Route path="/settings" element={<Settings navigate={ useNavigate()} />} />
       <Route path="/signup" element={<Game navigate={ useNavigate()} />} />
     </Routes>
     );
