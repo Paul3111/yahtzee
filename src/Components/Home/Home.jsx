487dc217581ab98954a56540dc179f4f8f3da741
ChangWynn <huynhchang.one@gmail.com>
2023-05-04 00:57:01 +0100

    new landing page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -5,6 +5,8 @@ import GameHeader from '../Game/JSX/GameHeader';
 import '../Game/CSS/GameHeader.module.css';
 import GameMenu from '../Game/JSX/GameMenu';
 
+import video from './videoBackground.mp4'
+
 const Home = () => {
   const navigate = useNavigate();
 
@@ -13,20 +15,37 @@ const Home = () => {
   }
 
   return (
-    <div className={`${style['god-container']}`}>
-      <GameMenu />
-      <div className={style['page-container']}>
-        <div className={style['game-header']}>
-        <GameHeader />
+    <div className={style['god-container']}>
+      {/* <GameMenu /> */}
+      <div className={style['video-container']}>
+        <video src={video} autoPlay loop muted ></video>
+      </div>
+      <div className={style['home-title__container']}>
+        <div className={`${style['home-title__letter-container']}  ${style['flickering']}`}>
+          <img src="/yahtzee_letters/Y.png" alt="" />
         </div>
-        <div className={style['btns-container']}>
-          <button className={style['enter-to-start']} 
-            onClick={gameRedirect}>
-            PLAY
-          </button>
+        <div className={`${style['home-title__letter-container']}`}>
+          <img src="/yahtzee_letters/A.png" alt="" />
         </div>
-
+        <div className={`${style['home-title__letter-container']}`}>
+          <img src="/yahtzee_letters/H.png" alt="" />
         </div>
+        <div className={`${style['home-title__letter-container']}`}>
+          <img src="/yahtzee_letters/T.png" alt="" />
+        </div>
+        <div className={`${style['home-title__letter-container']}`}>
+          <img src="/yahtzee_letters/Z.png" alt="" />
+        </div>
+        <div className={`${style['home-title__letter-container']} `}>
+          <img src="/yahtzee_letters/E.png" alt="" />
+        </div>
+        <div className={`${style['home-title__letter-container']}`}>
+          <img src="/yahtzee_letters/E.png" alt="" />
+        </div>
+      </div>
+      <button className={style['enter-to-start']} onClick={gameRedirect}>
+        PLAY
+      </button>
     </div>
   );
 };
