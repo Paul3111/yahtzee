69983ee6971e39fe516303f424827fcfec96d6f9
Paul Lazar <paullazar@MASTUDENT-Paul-Lazar-C17GMAXPQ6L4.local>
2023-05-04 01:43:34 +0100

    Test added - Finds ROLL button on game page.

--- cypress/e2e/game.cy.js
+++ cypress/e2e/game.cy.js
@@ -12,16 +12,26 @@ describe('<Game />', () => {
 })
 
   context('Buttons', () => {
-    xit('Goes to Home page', () => {
-      cy.url().should('eq', 'http://localhost:3000/home')
-    })
+    beforeEach(() => {
+      cy.visit('http://localhost:3000/game')
+      cy.on('window:confirm', (message) => {
+        cy.get('.start-game-popup__btn').click()
+        expect(message).to.equal('Ready?')
+        return true
+      })
+      })
 
-    xit('Finds YAHTZEE on the home page.', () => {
+    it('Finds YAHTZEE on the game page.', () => {
       cy.contains('YAHTZEE').should('be.visible')
     })
 
-    xit('Finds the play button text on the home page.', () => {
-      cy.contains('PLAY').should('be.visible')
+    it('Finds Home button on the game page.', () => {
+      cy.contains('YAHTZEE').should('be.visible')
+    })
+
+    it('Finds the first die on the game page.', () => {
+      cy.get('.wrapper').scrollIntoView().click();
+      cy.contains('ROLL').should('be.visible')
     })
 
     xit('Finds the Home button text on the home page.', () => {
@@ -36,10 +46,12 @@ describe('<Game />', () => {
       cy.contains('Multiplayer').should('be.visible')
     })    
 
-
-      // cy.wait(1000); // Wait for 1 second
-      // cy.get('.enter-to-start').should('contain', 'PLAY')
-      // cy.get('.navbar-link').scrollIntoView().click();
+    // cy.get('img[src="/path/to/image.png"]').should('have.attr', 'alt', 'My image')
+    // cy.get('img[src="/path/to/image.png"]').should('have.css', 'width', '100px')
+    // cy.get('img[src="/path/to/image.png"]').should('have.css', 'height', '100px') 
+    // cy.wait(1000); // Wait for 1 second
+    // cy.get('.enter-to-start').should('contain', 'PLAY')
+    // cy.get('.navbar-link').scrollIntoView().click();
 
 
   })
